bf90852b394324557beee53f3554265691c5a8e7
Joshua Bragge <joshuabragge@hotmail.com>
2025-01-31 11:05:35 -0700

    definietly fixed watch os background issue

--- Less Timer Watch App/SessionManager.swift
+++ Less Timer Watch App/SessionManager.swift
@@ -3,25 +3,55 @@ import OSLog
 
 #if os(watchOS)
 @MainActor
-class SessionManager: ObservableObject {
+class SessionManager: NSObject, ObservableObject, WKExtendedRuntimeSessionDelegate {
     private let logger = Logger.session
     
     @Published private(set) var isSessionActive = false
     @Published private(set) var startTime: Date?
-    
-    init() {}
+    private var extendedSession: WKExtendedRuntimeSession?
     
     func startSession() {
         guard !isSessionActive else { return }
+        
+        extendedSession = WKExtendedRuntimeSession()
+        extendedSession?.delegate = self
+        extendedSession?.start()
+        
         isSessionActive = true
         startTime = Date()
-        logger.info("startSession")
+        logger.info("Session started")
     }
     
     func stopSession() {
+        extendedSession?.invalidate()
+        extendedSession = nil
         isSessionActive = false
         startTime = nil
-        logger.info( "stopSession")
+        logger.info("Session stopped")
+    }
+    
+    // MARK: - WKExtendedRuntimeSessionDelegate
+    
+    nonisolated func extendedRuntimeSessionDidStart(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        Task { @MainActor in
+            logger.info("Extended runtime session started")
+        }
+    }
+    
+    nonisolated func extendedRuntimeSessionWillExpire(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        Task { @MainActor in
+            logger.info("Extended runtime session will expire")
+        }
+    }
+    
+    nonisolated func extendedRuntimeSession(_ extendedRuntimeSession: WKExtendedRuntimeSession,
+                               didInvalidateWith reason: WKExtendedRuntimeSessionInvalidationReason,
+                               error: Error?) {
+        Task { @MainActor in
+            isSessionActive = false
+            startTime = nil
+            logger.error("Session invalidated: \(reason.rawValue), error: \(String(describing: error))")
+        }
     }
 }
 
